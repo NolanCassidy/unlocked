93ee8465a4119f46eeb86f709f0a4ed9be2d7117
Nolan Cassidy <nolancassidy1@gmail.com>
2020-04-22 16:51:28 -0700

    remove keys

--- unlocked-app/client/src/services/firebase.js
+++ unlocked-app/client/src/services/firebase.js
@@ -3,14 +3,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 var config = {
-      apiKey: "AIzaSyDWFPUFmgidadIt_oFs3o6n5LlV2gznraM",
-      authDomain: "auth-a8c9d.firebaseapp.com",
-      databaseURL: "https://auth-a8c9d.firebaseio.com",
-      projectId: "auth-a8c9d",
-      storageBucket: "auth-a8c9d.appspot.com",
-      messagingSenderId: "63954932926",
-      appId: "1:63954932926:web:04cb682ccab08578ffbaef",
-      measurementId: "G-NQF4P2ZRZB"
+      apiKey: "XXXXXXXXXXXXXXXXXXXXXXXX",
+      authDomain: "XXXXXXXXXXXXXXXXXXXXXXXX",,
+      databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXX",,
+      projectId: "XXXXXXXXXXXXXXXXXXXXXXXX",,
+      storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXX",,
+      messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXX",,
+      appId: "XXXXXXXXXXXXXXXXXXXXXXXX",,
+      measurementId: "XXXXXXXXXXXXXXXXXXXXXXXX",
   };
 
 firebase.initializeApp(config);
